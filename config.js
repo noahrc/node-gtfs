2620a30e0dfa44615eac7df403349df345e5f46b
Noah Robison-Cox <noahrc@gmail.com>
2014-07-16 22:31:42 -0400

    add nj-bus to config

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ module.exports = {
   , agencies: [
       /* Put agency_key names from gtfs-data-exchange.com.  
       Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
-      'alamedaoakland-ferry'
-    , { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
+      'nj-bus'
+    /*, { agency_key: 'caltrain', url: 'http://www.gtfs-data-exchange.com/agency/caltrain/latest.zip'}
     , 'ac-transit'
     , 'county-connection'
     , 'san-francisco-municipal-transportation-agency'
     , 'bay-area-rapid-transit'
-    , 'golden-gate-ferry'
+    , 'golden-gate-ferry'*/
     ]
 }
